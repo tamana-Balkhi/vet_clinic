0eb4573e3d3d90c41be492b01535cd2ec7c8bce5
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-10-29 15:52:43 +0430

    created new table and alter animals table

--- schema.sql
+++ schema.sql
@@ -1,11 +1,41 @@
 CREATE TABLE animals (
-id INT,
-name VARCHAR(15) not null,
-date_of_birth date,
-escape_attempt INT,
-neutered BOOLEAN DEFAULT false,
-weight_kg FLOAT DEFAULT 0
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempt INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
 CREATE TABLE
 
 ALTER TABLE animals ADD species char(50);
+
+
+/* third day */
+
+ CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE TABLE
+
+
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id) 
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD owner_id INT;
+
+
+/*Set relation between owners and animals*/
+ALTER TABLE animals ADD CONSTRAINT fk_animals_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
+/*Set realtion between species and owners*/
+ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) REFERENCES species(id);
